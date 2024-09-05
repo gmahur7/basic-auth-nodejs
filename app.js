339adea2e28953fba948bb441c67902a1db5fc7e
gmahur7 <gm7017308@gamil.com>
2024-09-05 16:50:04 +0530

    next

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
 require('dotenv').config()
-const express=require('express')
+const express = require('express')
 const bodyParser = require('body-parser');
 const mongoDBConnect = require('./Database/dbConnect')
-const app=express()
+const app = express()
 mongoDBConnect()
-const userRoutes=require('./Routes/UserRoutes')
+const userRoutes = require('./Routes/UserRoutes')
 
-const cors=require('cors')
+const cors = require('cors')
 
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/',(req,resp)=>
-{
+app.get('/', (req, resp) => {
     resp.send('Hello World')
 })
 
 app.post('/webhook', (req, res) => {
+    console.log(req.body)
     const incomingMsg = req.body.Body;
     const senderNumber = req.body.From;
-  
-    console.log(`Received message: "${incomingMsg}" from ${senderNumber}`);
-  
-    // Process the message here
-    const responseMsg = `You said: ${incomingMsg}`;
-  
-    client.messages
-      .create({
-        body: responseMsg,
-        from: 'whatsapp:+14155238886', // Your Twilio WhatsApp number
-        to: 'whatsapp:+917017308602'
-      })
-      .then(message => console.log(`Message sent with SID: ${message.sid}`))
-      .catch(err => console.error('Error sending message:', err));
-  
-    res.sendStatus(200);
-  });
+    try {
+
+        console.log(`Received message: "${incomingMsg}" from ${senderNumber}`);
+        const responseMsg = `You said: ${incomingMsg}`;
+
+        client.messages
+            .create({
+                body: responseMsg,
+                from: 'whatsapp:+14155238886', // Your Twilio WhatsApp number
+                to: 'whatsapp:+917017308602'
+            })
+            .then(message => console.log(`Message sent with SID: ${message.sid}`))
+            .catch(err => console.error('Error sending message:', err));
+
+        return res.status(200).send({
+            success: true,
+            message: "Message send"
+        })
+    } catch (error) {
+        return res.status(500).send({
+            success: false,
+            message: error
+        })
+    }
+});
 
 //---------------------------USER ROUTES------------------------------------------------
-app.use("/api/user",userRoutes)
+app.use("/api/user", userRoutes)
 
-app.listen(port,()=>
-{
-    console.log("Server Is Running At "+port)
+app.listen(port, () => {
+    console.log("Server Is Running At " + port)
 })
